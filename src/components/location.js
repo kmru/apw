4da85efdf8b048d2e19e18100bda69713bd3f6b6
kmru <106931432+kmru@users.noreply.github.com>
2022-06-05 22:57:56 -0400

    Address card template for each store location

---
+++ src/components/location.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const Location = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.address_line_1}</p>
+      <p>{props.city_state_zip}</p>
+      <p>{props.url}</p>
+      <p>{props.hours}</p>
+    </div>
+  )
+}
+
+export default Location
