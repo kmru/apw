a424c71e6fde5c564c0ad951f87320826d9f3609
kmru <106931432+kmru@users.noreply.github.com>
2022-06-06 07:21:59 -0400

    Add fields

--- src/components/location.js
+++ src/components/location.js
@@ -6,8 +6,9 @@ const Location = (props) => {
       <h1>{props.name}</h1>
       <p>{props.address_line_1}</p>
       <p>{props.city_state_zip}</p>
-      <p>{props.url}</p>
       <p>{props.hours}</p>
+      <p>{props.url}</p>
+      <p>{props.phone}</p>
     </div>
   )
 }
