70403b0ca48a0c3553cb19df72f7f8472285ec9f
kmru <106931432+kmru@users.noreply.github.com>
2022-06-08 12:28:56 -0400

    Separate location data into its own file; style

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 import Header from '../components/header'
 import Event from '../components/event'
 import {siteUrl, links, logo, header_banner, event_banner} from './wh_data'
+import {locationsUrl, pageTitle} from './locations_data'
+
 
 const IndexPage = () => {
   return (
@@ -17,7 +19,7 @@ const IndexPage = () => {
         background_pic={event_banner}>
       </Event>
       <footer>
-        <p><Link to="/locations">Locations and Hours</Link></p>
+        <p><Link to={locationsUrl}>{pageTitle}</Link></p>
       </footer>
     </main>
   )
---
+++ src/pages/locations_data.js
@@ -0,0 +1,24 @@
+export const pageTitle = "Locations & Hours"
+export const locationsUrl = "/locations"
+export const locs = [
+  {
+    name: "Cooper's Cave Airsoft & Paintball Warehouse",
+    address_line_1: "1330 Saratoga Rd",
+    city_state_zip: "Gansevoort, NY 12831",
+    hours: ["Tuesday - Saturday: 11am - 7pm", "Sunday: 11am - 5pm"],
+    url: "https://www.cooperscavepaintball.com",
+    phone: "(518) 409-4547",
+  },
+  {
+    name: "Cooper's Cave Games",
+    address_line_1: "1607 Route 9",
+    city_state_zip: "Fort Edward, NY 12828",
+    hours: [
+      "Monday: 10am - 9pm",
+      "Tuesday - Thursday: 10am - 7pm",
+      "Friday: 10am - 9pm",
+      "Saturday & Sunday: 10am - 7pm"],
+    url: "https://www.cooperscavegames.com",
+    phone: "(518) 745-4263",
+  },
+]
