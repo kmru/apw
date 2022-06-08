6df190946d6c65bdc88593b493bc097e9f16be5b
kmru <106931432+kmru@users.noreply.github.com>
2022-06-08 11:17:09 -0400

    Separate constant data into its own files

---
+++ src/pages/games_data.js
@@ -0,0 +1,54 @@
+import games_logo from '../images/games_logo.jpeg'
+
+export const siteUrl = "https://www.cooperscavegames.com"
+export const logo = games_logo
+export const links = [
+  {
+    text: "Magic: The Gathering",
+    url: "/mtg",
+    description:
+      "MTG singles, collection, accessories: card sleeves, deck boxes.",
+  },
+  {
+    text: "Pokémon and Other TCGs",
+    url: "/pokemon-and-tcgs",
+    description:
+      "Pokémon The Card Game, Flesh and Blood, Yu-Gi-Oh!",
+  },
+  {
+    text: "Warhammer",
+    url: "/warhammer",
+    description:
+      "Miniature wargaming models, codexes, paints, terrain, and assembly tools.",
+  },
+  {
+    text: "Video Games",
+    url: "/videogames",
+    description:
+      "",
+  },
+  {
+    text: "Action Figures & Toys",
+    url: "/action-figures-and-toys",
+    description:
+      ".",
+  },
+  {
+    text: "Tabletop Games",
+    url: "/tabletop-games",
+    description:
+      ".",
+  },
+  {
+    text: "Comic Books & Graphic Novels",
+    url: "/comic-books",
+    description:
+      ".",
+  },
+  {
+    text: "Airsoft & Paintball",
+    url: "/paintball-and-airsoft",
+    description:
+      ".",
+  },
+]
---
+++ src/pages/wh_data.js
@@ -0,0 +1,18 @@
+import warehouse_logo from '../images/wh_primary_logo.jpg'
+
+export const siteUrl = "https://www.cooperscavepaintball.com"
+export const logo = warehouse_logo
+export const links = [
+  {
+    text: "Airsoft",
+    url: "/airsoft",
+    description:
+      "Airsoft information, blah blah blah.",
+  },
+  {
+    text: "Paintball",
+    url: "/paintball",
+    description:
+      "Paintball information, blah blah blah.",
+  },
+]
